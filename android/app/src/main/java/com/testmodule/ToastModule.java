de13f850184f12a29cfad78bbacde5e6e4902913
Qtron <Qtron@protonmail.com>
2017-08-11 03:22:55 +0300

    Fix back button, add native modules for icons

--- android/app/src/main/java/com/testmodule/ToastModule.java
+++ android/app/src/main/java/com/testmodule/ToastModule.java
@@ -1,5 +1,9 @@
 package com.testmodule;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Environment;
 import android.text.TextUtils;
 import android.widget.Toast;
@@ -8,13 +12,12 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.ReadableNativeArray;
 import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -61,6 +64,18 @@ public class ToastModule extends ReactContextBaseJavaModule {
         resultCallback.invoke(uris);
     }
 
+    @ReactMethod
+    public void getFilesMap(String currentUri, Callback resultCallback) {
+        File[] files;
+        if (TextUtils.isEmpty(currentUri)) {
+            files = root.listFiles();
+        } else {
+            files = new File(currentUri).listFiles();
+        }
+        WritableMap uris = getUriMap(files);
+        resultCallback.invoke(uris);
+    }
+
     private WritableArray getUriList(File[] files) {
         WritableArray uris = new WritableNativeArray();
         if(files == null){
@@ -72,6 +87,29 @@ public class ToastModule extends ReactContextBaseJavaModule {
         return uris;
     }
 
+    private WritableMap getUriMap(File[] files) {
+        WritableMap uris = new WritableNativeMap();
+        if(files == null){
+            return uris;
+        }
+        for (File file : files) {
+            if(file.isDirectory()) {
+                uris.putString("icon_uri", getFolderIconUri());
+            } else {
+                uris.putString("icon_uri", getFileIconUri(file.getPath()));
+            }
+            uris.putBoolean("directory", file.isDirectory());
+            uris.putString("uri", file.getPath());
+        }
+        return uris;
+    }
+
+    private String getFolderIconUri(){
+        String packageName = getReactApplicationContext().getPackageName();
+        String fileName = "ic_folder_icon";
+        return "android.resource://" + packageName + "/drawable/" + fileName;
+    }
+
     @ReactMethod
     public void show(String message, int duration) {
         if (toast != null && toast.getView().isShown()) {
@@ -79,5 +117,26 @@ public class ToastModule extends ReactContextBaseJavaModule {
         }
         toast = Toast.makeText(getReactApplicationContext(), message, duration);
         toast.show();
+    }
+
+    private String getFileIconUri(String fileUri){
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(fileUri));
+        intent.setType("application/pdf");
+
+        final List<ResolveInfo> matches = getReactApplicationContext().getPackageManager().queryIntentActivities(intent, 0);
+        if(!matches.isEmpty()){
+            return getIconUri(matches.get(0).activityInfo).toString();
+        }
+        return "";
+    }
+
+    @ReactMethod
+    public void getFileIcon(String fileUri, Callback resultCallBack){
+         resultCallBack.invoke(getFileIconUri(fileUri));
+    }
+
+    private Uri getIconUri(ActivityInfo appInfo){
+        return Uri.parse("android.resource://" + appInfo.packageName + "/" + appInfo.applicationInfo.icon);
     }
 }
--- index.android.js
+++ index.android.js
@@ -4,79 +4,97 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  FlatList,
-  List,
-  SearchBar,
-  BackHandler
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Button,
+    FlatList,
+    List,
+    SearchBar,
+    BackHandler
 } from 'react-native';
 
-import { NativeModules } from 'react-native'; 
+import {NativeModules} from 'react-native';
 
 export default class TestProject1 extends Component {
-	
-	constructor(props){
-		super(props);
-		this.state = {
-			data: ["Item1", "Item2"],
-			uri: "root"
-		};
-	}
-	
-	updateData(newData){
-		this.setState({ data: newData});
-	}
-	
-	_getParent(uri){
-		var parts = uri.split("\/");
-		parts.splice(-2, 2);
-		return parts.join("\/");
-	}
-	
-	_getFileName(uri){
-		var parts = uri.split("\/");
-		return parts.pop();
-	}
-	
-	componentDidMount(){
-		
-		BackHandler.addEventListener('hardwareBackPress', function() {  
-			this._goToParent(this.state.uri)
-			return true; 
-		});
-		
-		this._getRootFile();
-	}
-	
-	_getRootFile(){
-		NativeModules.MyToastAndroid.getFilesList("", (list) => {
-			this.updateData(list);
-		})
-	}
-	
-	
-	_goToParent(uri){
-		this._goToFile(this._getParent(uri));
-	}
-	
-	_goToFile(newUri){
-		NativeModules.MyToastAndroid.getFilesList(newUri, (list) => {
-			if (typeof list !== 'undefined' && list.length > 0) {
-				this.updateData(list);
-				this.state.uri = newUri;
-			} else {
-				//NativeModules.MyToastAndroid.show("No items found", 0);
-			}
-		})
-	}
-	
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            uri: "root"
+        };
+        this.backEventListener = this.backEventListener.bind(this);
+    }
+
+    updateData(newData) {
+        console.log("New uri is: " + newData);
+        // NativeModules.MyToastAndroid.show("New uri is: " + newUri, 0);
+        this.setState({data: newData});
+    }
+
+    updateUri(newUri) {
+        this.setState({uri: newUri});
+        NativeModules.MyToastAndroid.show("New uri is: " + newUri, 0);
+    }
+
+    _getParent(uri) {
+        var parts = uri.split("\/");
+        parts.splice(-2, 2);
+        return parts.join("\/");
+    }
+
+    _getFileName(uri) {
+        var parts = uri.split("\/");
+        return parts.pop();
+    }
+
+    getLastStateUri() {
+        return this.state.uri;
+    }
+
+    backEventListener() {
+        // NativeModules.MyToastAndroid.show(this.getLastStateUri(), 0);
+        this._goToParent(this.getLastStateUri());
+        return true;
+    }
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.backEventListener);
+        this._getRootFile();
+    }
+
+    _getRootFile() {
+        NativeModules.MyToastAndroid.getFilesList("", (list) => {
+            this.updateData(list);
+        })
+    }
+
+    _goToParent(uri) {
+        this._goToFile(this._getParent(uri));
+    }
+
+    _goToFile(newUri) {
+        NativeModules.MyToastAndroid.getFilesList(newUri, (list) => {
+            if (typeof list !== 'undefined' && list.length > 0) {
+                this.updateData(list);
+                this.updateUri(newUri);
+            } else {
+
+            }
+        })
+    }
+
+    _getFileIconUri(){
+        NativeModules.MyToastAndroid.getFileIcon(newUri, (iconUri) => {
+            return iconUri;
+        })
+    }
+
 //	renderHeader = () => {
 //		return <SearchBar placeholder="Type Here..." lightTheme round />;
 //		return <Text 
@@ -84,52 +102,62 @@ export default class TestProject1 extends Component {
 //					this._goToParent(this.state.uri)}
 //		>"..."</Text>;
 //	};
-	
-	renderHeader = () => {
-		return <SearchBar placeholder="Type Here..." lightTheme round />;
-	};
-	
-	
-	_keyExtractor = (item, index) => item;
-	
-  render() {
-    return (
-		<FlatList
-			data={this.state.data}
-			renderItem={({item}) => <Text 
-				style={styles.item}
-				onPress={(id) => {
-					this._goToFile(item);
-					//NativeModules.MyToastAndroid.show(this._getParent(item).toString(), 0);
-				}}
-			>{item}</Text>}
-		/>
-    );
-  }
+
+    renderHeader = () => {
+        return <SearchBar placeholder="Type Here..." lightTheme round/>;
+    };
+
+
+    _keyExtractor = (item, index) => item;
+
+    render() {
+        return (
+            <FlatList
+                data={this.state.data}
+                renderItem={({item}) =>
+                    <View>
+                        <Image
+                            source={require('./img/favicon.png')}
+                        />
+                        <Image
+                            style={{width: 50, height: 50}}
+                            source={{uri: this._getFileIconUri()}}
+                        />
+                        <Text
+                            style={styles.item}
+                            onPress={(id) => {
+                                this._goToFile(item);
+                                //NativeModules.MyToastAndroid.show(this._getParent(item).toString(), 0);
+                            }}
+                        >{item}</Text>
+                    </View>}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  item: {
-	  padding: 10,
-	  fontSize: 18,
-	  height: 80,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 80,
+    },
 });
 
 AppRegistry.registerComponent('TestProject1', () => TestProject1);
